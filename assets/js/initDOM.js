3e2708c7790730a68b61f499b79b8d0f108e11bb
konnn04 <trieukon1011@gmail.com>
2026-07-17 21:45:21 +0700

    feat: init

---
+++ assets/js/initDOM.js
@@ -0,0 +1,24 @@
+// function main() {
+//     const header = document.getElementById("header")
+//     fetch("/components/header.html").then(r => r.text()).then((v) => {
+//         header.innerHTML = v;
+//     }).catch((e)=> alert("Lỗi trang"))
+// }
+
+// // Chờ web load xong mới chạy
+// document.addEventListener("DOMContentLoaded", main) 
+
+
+function loadComponent(id, file) {
+    fetch(file).then(r => r.text()).then((v) => {
+        document.getElementById(id).innerHTML = v;
+    }).catch((e) => alert("Lỗi trang"))
+}
+
+
+function main() {
+    loadComponent("header", "components/header.html");
+    // loadComponent("footer", "components/footer.html");
+}
+
+document.addEventListener("DOMContentLoaded", main) 
